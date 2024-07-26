9344fcbc00424127599a3e9cbeeee01413c9eb8d
“Tsachi <thacmon@amazon.ch>
2024-07-26 16:38:26 +0200

    addAuth to dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,28 @@
-import SideNav from "@/app//ui/dashboard/sidenav";
+"use client"
 
+import SideNav from "@/app//ui/dashboard/sidenav";
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <Authenticator>
+      {({ signOut, user }) => (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
-  );
+  )
+}
+</Authenticator>
+  )
 }
