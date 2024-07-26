889f2b69ac06d9404bdc3bc6d801463861b01f49
“Tsachi <thacmon@amazon.ch>
2024-07-26 15:43:42 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -8,9 +6,7 @@ import Image from 'next/image';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
 
 export default function Page() {
   return (
