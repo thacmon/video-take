b8a74a1ff4751f7cecd3dd2c3fea1c96bddfe0c5
“Tsachi <thacmon@amazon.ch>
2024-08-06 16:02:36 +0200

    t

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -30,8 +30,8 @@ export default function SideNav() {
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
-          <button className="block md:hidden" onClick={handleSignOut}>Exit</button>
-        </form>
+         </form>
+         <button className="block md:hidden" onClick={handleSignOut}>Exit</button>
       </div>
     </div>
   );
