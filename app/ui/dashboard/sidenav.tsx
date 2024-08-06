7e489a4400caee6624602076fa1141560938c88c
“Tsachi <thacmon@amazon.ch>
2024-08-06 15:22:17 +0200

    ft

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,8 +2,12 @@ import Link from 'next/link';
 import NavLinks from './nav-links';
 import AcmeLogo from '../acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { signOut } from "aws-amplify/auth"
 
 export default function SideNav() {
+  async function handleSignOut() {
+    await signOut()
+  }
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,7 +22,7 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button onClick={handleSignOut} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
